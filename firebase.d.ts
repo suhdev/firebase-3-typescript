e2cb4c5561188534a5de023fa56d8236b4cea2d6
Uri Shaked <uri@salsa4fun.co.il>
2016-06-24 16:46:48 +0300

    fix a typo: blog -> blob

--- firebase.d.ts
+++ firebase.d.ts
@@ -491,11 +491,11 @@ declare namespace firebase {
         /**
          * (description)
          * 
-         * @param {Blob} blog (description)
+         * @param {Blob} blob (description)
          * @param {UploadMetadata} metadata (description)
          * @returns {UploadTask} (description)
          */
-        put(blog:Blob|File, metadata?:UploadMetadata):UploadTask;
+        put(blob:Blob|File, metadata?:UploadMetadata):UploadTask;
     }
     
     /**
